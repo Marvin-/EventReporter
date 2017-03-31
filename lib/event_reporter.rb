5cd3fbb6d515e8b08e16dcbb3ca69384f6c0098b
Marvin M <marvin23m@gmail.com>
2017-03-31 15:12:45 +0100

    Expand on commands hash by adding descriptions. Extend loop functionality

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,26 +1,33 @@
-require "csv"
 require_relative "methods.rb"
+
 COMMANDS = {
-  load: "Accepts <filename> option. Erase any loaded data and parse the specified file. If no filename is given, it will default to event_attendees.csv.
-", 
-  help: "Output a listing of the available individual commands. Accepts a <command> option which outputs a description of how to use the specific command.",
+  load: "Accepts <filename> option. 
+         Erase any loaded data and parse the specified file. 
+         If no filename is given, it will default to event_attendees.csv.", 
+  help: "Output a listing of the available individual commands. 
+         Accepts a <command> option which outputs a description of how to use the specific command.",
   find: "Finds all records matching the criteria for the given attribute.", 
+  queue: "",
   exit: "Exits program."
 }
 
-loop do   
+loop do
   puts "Please enter a command"
 
-  command = get_input
+  command = get_command
+  first_command = command.first
+  minus_first_command = command.drop(1).join(" ")
 
-  case command
-  when :load
+  case first_command
+  when "load"
     load
-  when :help
-    help
-  when :find
+  when "help"
+    help minus_first_command
+  when "find"
     find
-  when :exit
+  when "queue"
+    queue minus_first_command
+  when "exit"
     exit
   end
 end
